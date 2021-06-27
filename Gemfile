fe54c6d3862094cb14c4dbf1e98d3fea74f7c373
Ubuntu <ubuntu@ip-172-31-29-137.us-east-2.compute.internal>
2021-06-27 21:30:52 +0000

    finish

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jbuilder', '~> 2.7'
 # gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
+
+gem 'bootstrap-sass', '3.4.1'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,21 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+function showTime(){
+  myDate = new Date();
+  hours = myDate.getHours();
+  minutes = myDate.getMinutes();
+  seconds = myDate.getSeconds();
+  if (hours < 10) hours = 0 + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+  var h4 = document.getElementById("horas").innerHTML = hours+ ":" +minutes+ ":" +seconds
+  
+  anioActual = myDate.getFullYear()
+  mesActual = myDate.getMonth()+1
+  
+   var h4Fecha = document.getElementById("fecha").innerHTML= mesActual+"/"+anioActual
+  
+  setTimeout("showTime()", 1000);
+
+}
+showTime()
