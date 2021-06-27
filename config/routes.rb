5f8c385ef495b335f7b718a6443df01af0defe40
Ubuntu <ubuntu@ip-172-31-29-137.us-east-2.compute.internal>
2021-06-27 18:41:22 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/basics'
   get 'static_pages/ruby'
   get 'static_pages/git'
-    root 'application#hola'
+    root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
