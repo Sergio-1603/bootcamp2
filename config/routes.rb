245eb5eb879fb20437547965c25fc7a7d93a4784
Ubuntu <ubuntu@ip-172-31-29-137.us-east-2.compute.internal>
2021-06-27 20:27:44 +0000

    fixing the routes again

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/basics'
-  get 'static_pages/ruby'
-  get 'static_pages/git'
+  get '/home', to: 'static_pages#home'
+  get '/basics', to: 'static_pages#basics'
+  get '/git', to: 'static_pages#git'
+  get '/ruby', to: 'static_pages#ruby'
     root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
