0f95006f172087e0c632f898e92b256ef79080f5
Ubuntu <ubuntu@ip-172-31-29-137.us-east-2.compute.internal>
2021-06-28 16:44:09 +0000

    adding user to my project

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/basics', to: 'static_pages#basics'
   get '/git', to: 'static_pages#git'
   get '/ruby', to: 'static_pages#ruby'
+  get '/signup', to: 'users#new'
     root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
